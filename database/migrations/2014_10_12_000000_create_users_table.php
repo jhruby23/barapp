ef540023edd77eb1a0872a4842ba528d0c292e97
Honza <jhruby@meds.cz>
2016-02-16 17:25:58 +0100

    DB structure

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,9 +14,11 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password', 60);
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->integer('code')->unique();
+            $table->integer('rfid_code')->unique()->nullable();
+            $table->string('rfid_hex')->unique()->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
